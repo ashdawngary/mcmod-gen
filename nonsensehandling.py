05bef03d905e9fe772591b586707a40cc7ec4c5d
Naor <crhui.dsuk@gmail.com>
2019-05-13 19:18:46 -0400

    added functions to nonsensehandeling

--- nonsensehandling.py
+++ nonsensehandling.py
@@ -1,2 +1,21 @@
 #####################
 # literaly preprocess the code for the mistakes and stuff that we can quick fix.
+#####################
+
+
+def rmvGarbage(line):
+    line.lstrip("")
+    return line
+
+
+def removeWord(line):
+    i = line.find(" ")
+    line = line[i + 1:]
+    line.lstrip()
+    return line
+
+
+def removeSpaces(line):
+    line = line.rstrip()
+    line = line.lstrip()
+    return line
